a2e518d61b984f9c3e1c1e2eacb1b2e28f32a09d
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-05-09 23:06:33 +0530

    added post comment like functionalites

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,65 +1,260 @@
-"use client"
-import React from 'react'
-import {useSession} from "next-auth/react"
-
-const Post = ({img,userImg,username,caption,id}) => {
-  const {data:session} = useSession();
+"use client";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import Moment from "react-moment";
+import { db } from "../../firebase";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+const Post = ({ img, userImg, username, caption, id }) => {
+  const { data: session } = useSession();
+  console.log(session);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", id, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+  useEffect(() => {
+    setHasLiked(likes.findIndex((like) => like.id === session.user?.uid) !== -1);
+  }, [likes]);
+  async function likePost() {
+    if (hasLiked) {
+      await deleteDoc(doc(db, "posts", id, "likes", session.user?.uid));
+    } else {
+      await setDoc(doc(db, "posts", id, "likes", session.user?.uid), {
+        username: session.user?.username,
+      });
+    }
+  }
+  async function sendComment(event) {
+    event.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user?.username,
+      userImage: session.user?.image,
+      timestamp: serverTimestamp(),
+    });
+  }
   return (
-    
     <div className="my-8 bg-white border">
-    <div className="flex items-center justify-between p-4 xs:p-6">
-       <img className="h-12 p-1 mr-2 border rounded-full" src={userImg} alt="user"/>
+      <div className="flex items-center justify-between p-4 xs:p-6">
+        <img
+          className="h-12 p-1 mr-2 border rounded-full"
+          src={userImg}
+          alt="user"
+        />
         <p className="flex-1 font-bold">{username}</p>
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+          />
         </svg>
       </div>
-  
-   <div className="flex items-center justify-center w-full">
-    <img className="object-cover w-full" src={img} alt="nature"/>
-   </div>
-   {session && ( 
-   <div className="flex items-center justify-between px-4 pt-4 pb-1">
-    <div className="flex items-center justify-center gap-2 p-1">
-      <div className="flex items-center justify-center p-1">
-      <svg xmlns="http://www.w3.org/2000/svg" className="btn" fill="red" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="0">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-      </svg></div>
-      <div className="flex items-center justify-center p-1">
-      <svg xmlns="http://www.w3.org/2000/svg" className="btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-      </svg></div>
-      <div className="flex items-center justify-center p-1 rotate-45">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-      </svg></div>
-    </div>
-    <div className="flex items-center justify-center p-1">
-      <svg xmlns="http://www.w3.org/2000/svg" className="btn" fill="black" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="0">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-      </svg>
-    </div>
-   </div>)}
-   <div className="flex flex-col items-start justify-center gap-1 p-4 truncate xs:p-6">
-  <div className="flex items-center justify-start">
-    <span className="mr-2 font-bold truncate">{username}</span>
-    <p className="truncate">{caption}</p>
-    </div>
-  </div>
-{session && (
-<form className="flex items-center justify-between gap-1 p-4 xs:p-6">
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-  <input className="flex-1 border-none focus:ring-0" type="text" placeholder="Enter your comment" />
-  <button className="font-bold lg:flex hidden text-blue-500">Post</button>
-  <div className="flex items-center lg:hidden justify-center p-1 rotate-45">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 btn" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-      </svg></div>
-</form>)}
-</div>
-  )
-}
 
-export default Post
+      <div className="flex items-center justify-center w-full">
+        <img className="object-cover w-full" src={img} alt="nature" />
+      </div>
+      {session && (
+        <div className="flex items-center justify-between px-4 pt-4 pb-1">
+          <div className="flex items-center justify-center gap-2 p-1">
+            {hasLiked ? (
+              <div onClick={likePost} className="flex cursor-pointer items-center justify-center p-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="btn"
+                  fill="red"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="0"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <div onClick={likePost} className="flex cursor-pointer items-center justify-center p-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </svg>
+              </div>
+            )}
+            <div className="flex items-center justify-center p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="btn"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                />
+              </svg>
+            </div>
+            <div className="flex items-center justify-center p-1 rotate-45">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 btn"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="flex items-center justify-center p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="btn"
+              fill="black"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="0"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+              />
+            </svg>
+          </div>
+        </div>
+      )}
+      <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="font-bold mb-1">{likes.length} likes</p>
+        )}
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+      {comments.length > 0 && (
+        <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+          {comments.map((comment) => (
+            <div
+              key={comment.data().id}
+              className="flex items-center space-x-2 mb-2"
+            >
+              <img
+                className="h-7  rounded-full object-cover"
+                src={comment.data().userImage}
+                alt="user-image"
+              />
+              <p className="font-semibold">{comment.data().username}</p>
+              <p className="flex-1 truncate">{comment.data().comment}</p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
+      {session && (
+        <form className="flex items-center justify-between gap-1 p-4 xs:p-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+            className="border-none flex-1 focus:ring-0"
+            type="text"
+            placeholder="Enter your comment..."
+          />
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="text-blue-400 font-bold disabled:text-blue-200"
+          >
+            Post
+          </button>
+          <div className="flex items-center lg:hidden justify-center p-1 rotate-45">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 btn"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+              />
+            </svg>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Post;
